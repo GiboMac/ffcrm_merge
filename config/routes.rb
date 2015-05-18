60757f418463904b239b4ea23f84591a06bd3807
Gilberto <gilby87@gmail.com>
2015-05-18 12:02:35 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  match 'merge/:klass_name/:duplicate_id/into/:master_id', :controller => 'merge', :action => 'into', :as => :into_merge, via: :all
-  match 'merge/:klass_name/aliases', :controller => 'merge', :action => 'aliases', :as => :aliases_merge, via: :all
+  put  'merge/:klass_name/:duplicate_id/into/:master_id', :controller => 'merge', :action => 'into', :as => :into_merge
+  post 'merge/:klass_name/aliases', :controller => 'merge', :action => 'aliases', :as => :aliases_merge
 
 end
